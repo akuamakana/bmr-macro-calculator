fc1028d869a1ce97d7dd22e37b6a5809c7b4eea6
Akuamakana <akuamakana@pop-os.localdomain>
2020-12-26 16:59:43 -0800

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container grid grid-cols-2">
+    <div class="grid grid-rows-3">
+      <Stats />
+      <ExerciseLevel />
+      <Goal />
+    </div>
+    <div>
+      <Results />
+    </div>
+  </div>
+</template>
+
+<script>
+import Stats from '~/components/Stats'
+import ExerciseLevel from '~/components/ExerciseLevel'
+import Goal from '~/components/Goal'
+import Results from '~/components/Results'
+export default {
+  components: Stats, ExerciseLevel, Goal, Results
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,6 @@
+export const state = () => ({
+  gender: 'male',
+  age: 0,
+  height: 69,
+  weight: 200
+})
